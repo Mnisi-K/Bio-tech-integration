37543845e112cfaeefb92947eda8dec56810e6c1
Mr Mnisi <momasseminent@gmail.com>
2026-02-08 21:57:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -132,10 +132,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   els.forEach(el => io.observe(el));
 });
-const navToggle = document.getElementById("nav-toggle");
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    document.getElementById("main-nav")?.classList.toggle("open");
-  });
-}
-
